19750defd09f357eb116f8f0c74536409e68cf8f
matanad <31176421+matanad@users.noreply.github.com>
2022-10-25 19:13:18 +0300

    Add files via upload

---
+++ js/day4.js
@@ -0,0 +1,57 @@
+var ball
+var btnShape
+var measure = 'px'
+
+init()
+
+function init() {
+    if (ball === undefined) {
+        ball = document.querySelector('.ball')
+    }
+
+    if (btnShape === undefined) {
+        btnShape = document.querySelector('.shape-btn')
+    }
+}
+
+function changeTextColor() {
+    init()
+    ball.style.color = '#00600f'
+}
+
+function changeTextSize() {
+    init()
+    var maxSize = 50
+    var size = prompt('Enter text size')
+
+    if (size <= maxSize) {
+        ball.style.fontSize = size + measure
+    } else {
+        console.log('Too big!')
+    }
+}
+
+var isRound = true
+
+function toggleBallShape() {
+    init()
+    if (isRound) {
+        ball.style.borderRadius = '0%'
+        btnShape.innerText = 'square'
+        ball.innerText = 'I am a square'
+        isRound = !isRound
+    } else {
+        ball.style.borderRadius = '50%'
+        btnShape.innerText = 'round'
+        ball.innerText = 'I am a ball'
+        isRound = !isRound
+    }
+}
+
+var ballMargin = 35
+
+function moveDown() {
+    init()
+    ball.style.marginTop = ballMargin + measure
+    ballMargin += 5
+}
---
+++ js/day5.js
@@ -0,0 +1,88 @@
+function getLabelValue(elLabel) {
+    var value = document.querySelector(elLabel).value
+    return value
+}
+
+/**  
+ *   Before getMult() made
+ */
+// function printMult() {
+//     var num1 = getLabelValue('.firstNum')
+//     var num2 = getLabelValue('.second-num')
+//     var mult = num1 * num2
+//     ball.innerText = mult
+// }
+
+
+/**
+ * Prints the multiplication of the input number on the ball
+ */
+function printMult() {
+    ball.innerText = "The mult is: " + getMult()
+}
+
+/**
+ * Get the value of the inputs first-num and second-num
+ * and returns the multiplication of them.
+ * @returns Result of the multiplication of num1 and num2
+ */
+function getMult() {
+    var num1 = getLabelValue('.firstNum')
+    var num2 = getLabelValue('.second-num')
+    var res = num1 * num2
+    return res
+}
+
+var names = ["Shuki", "Muki", "Puki", "Kuki", "Shituki"]
+// arrayOfStrings(names)
+
+/**
+ * 
+ * @param {array strings} arr 
+ * Task 3
+ */
+function arrayOfStrings(arr) {
+    console.log("arr.lenght: ", arr.length);
+    console.log("arr[1]: ", arr[1]);
+
+    console.log("for loop:");
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        if (arr[i] === "Puki") {
+            console.log("Hey! i Know You!");
+        }
+    }
+}
+
+var numbers = [30, 6, 22, 8, 13, 90]
+printArray(numbers)
+console.log("Sum: " + getSum(numbers))
+
+/**
+ * 
+ * @param {array numbers} arr 
+ * Task 4
+ */
+function printArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 10) {
+            console.log("arr[" + i + "] = " + arr[i]);
+        } else {
+            arr[i] *= 10
+            console.log("arr[" + i + "] * 10 = " + arr[i]);
+        }
+    }
+}
+
+/**
+ * Task 5
+ * @param {array numbers} nums 
+ * @returns The sum of all the numbers in the array
+ */
+function getSum(nums) {
+    var sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    return sum
+}
